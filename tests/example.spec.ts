231f6afe2bf0576d52cb2f41a8467085eaed7f01
Andrei Pisarev <a.p@li.ru>
2023-04-10 14:15:17 +0300

    Added import to test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
+import { testValue } from './utils';
 
 test('has title', async ({ page }) => {
+  console.log(testValue);
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
